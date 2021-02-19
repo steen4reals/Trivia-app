d8068e9e55bdf6184bd4dd2bbe4998c85547d802
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-19 15:56:51 +0000

    styling

--- quiz-app/src/Components/EndGame/index.js
+++ quiz-app/src/Components/EndGame/index.js
@@ -1,8 +1,8 @@
 function EndGame({ score }) {
   return (
     <div className="App">
-      <h1>Game Over</h1>
-      <p>Your score is {score}</p>
+      <h1>{score >= 5 ? "You are epic, you truly belong among the Gods!" : "Congratulations, you are the God of Stupidity."}</h1>
+      <p>Final score: {score}</p>
     </div>
   );
 }
