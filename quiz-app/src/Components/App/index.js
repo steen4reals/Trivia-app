b500d91a351064676528403f877350f4e0b3344d
Liam Morley <liammorley123@live.co.uk>
2021-02-19 14:21:37 +0000

    messed around with bg

--- quiz-app/src/Components/App/index.js
+++ quiz-app/src/Components/App/index.js
@@ -1,52 +1,61 @@
-import { useReducer } from 'react';
-import useFetch from '../../hooks/useFetch';
-import Button from '../Button';
-import Screen from '../Screen/index';
-import './App.css';
+import { useReducer } from "react";
+import useFetch from "../../hooks/useFetch";
+import Button from "../Button";
+import Screen from "../Screen/index";
+import "./App.css";
+import EndGame from "../EndGame";
 
 const initialState = { index: 0, score: 0 };
 
 function reducer(state, action) {
-	let newScore = state.score;
-	if (action.correctAnswer === action.givenAnswer) {
-		newScore++;
-	}
-  console.log(state.score)
-	switch (action.type) {
-		case 'True':
-			return { index: state.index + 1, score: newScore };
-		case 'False':
-			return { index: state.index + 1, score: newScore };
-		default:
-			return state;
-	}
+  let newScore = state.score;
+  if (action.correctAnswer === action.givenAnswer) {
+    newScore++;
+  }
+  console.log(state.score);
+  switch (action.type) {
+    case "True":
+      return { index: state.index + 1, score: newScore };
+    case "False":
+      return { index: state.index + 1, score: newScore };
+    default:
+      return state;
+  }
 }
 
 function App() {
-	const [ state, dispatch ] = useReducer(reducer, initialState);
-	const data = useFetch();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const data = useFetch();
 
-	if (!data) {
-		console.log(`This is in IF STMT: ${data}`);
-		console.log(state);
-		return <h1>...Loading</h1>;
-	}
+  if (!data) {
+    console.log(`This is in IF STMT: ${data}`);
+    console.log(state);
+    return <h1>...Loading</h1>;
+  }
 
-	if (state.index >= data.length) {
-		return <h2>Game is over</h2>;
-	}
+  if (state.index >= data.length) {
+    return <EndGame score={state.score} />;
+  }
 
-	console.log(`This is in app: ${data[0]}`);
-	return (
-		<div className='App'>
-			<h1>Trials of Mt. Olympus</h1>
-			<Screen question={data[state.index].question} />
-			<div className='button-container'>
-				<Button value='True' dispatch={dispatch} correctAnswer={data[state.index].correct_answer} />
-				<Button value='False' dispatch={dispatch} correctAnswer={data[state.index].correct_answer}/>
-			</div>
-		</div>
-	);
+  console.log(`This is in app: ${data[0]}`);
+  return (
+    <div className="App">
+      <h1>Trials of Mt. Olympus</h1>
+      <Screen question={data[state.index].question} />
+      <div className="button-container">
+        <Button
+          value="True"
+          dispatch={dispatch}
+          correctAnswer={data[state.index].correct_answer}
+        />
+        <Button
+          value="False"
+          dispatch={dispatch}
+          correctAnswer={data[state.index].correct_answer}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- quiz-app/src/Components/EndGame/index.js
+++ quiz-app/src/Components/EndGame/index.js
@@ -1,5 +1,10 @@
-function EndGame() {
-  return <div></div>;
+function EndGame({ score }) {
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <p>Your score is {score}</p>
+    </div>
+  );
 }
 
 export default EndGame;
