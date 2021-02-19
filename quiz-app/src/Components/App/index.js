1271fbcd76a252147f3f1715712a76c13b9e9efa
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-19 13:33:40 +0000

    working

--- quiz-app/src/Components/App/index.js
+++ quiz-app/src/Components/App/index.js
@@ -4,33 +4,34 @@ import { useEffect, useState, useReducer } from "react";
 import Button from "../Button";
 import useFetch from "../../hooks/useFetch";
 
-const initialState = { index: 0 };
+const initialState =  0;
 
 function reducer(state, action) {
   switch (action.type) {
     case "true":
-      return state.index + 1;
+      return state + 1;
     case "false":
-      return state.index - 1;
+      return state + 1;
     default:
-      return state.index;
+      return state;
   }
 }
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const data = useFetch();
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  if (!data || !data[state.index]) {
+  if (!data || !data[state]) {
     console.log(`This is in IF STMT: ${data}`);
+    console.log(state)
     return <h1>...Loading</h1>;
   }
+
   console.log(`This is in app: ${data[0]}`);
   return (
     <div className="App">
       <h1>Trials of Mt. Olympus</h1>
-      <Screen question={data[state.index].question} />
+      <Screen question={data[state].question} />
       <div className="button-container">
         <Button value="true" dispatch={dispatch} />
         <Button value="false" dispatch={dispatch} />
