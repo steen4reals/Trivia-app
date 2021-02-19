809d85fe97d56093baba624598a9f1ceab4fd45d
Liam Morley <liammorley123@live.co.uk>
2021-02-19 15:16:40 +0000

    style time

--- quiz-app/src/Components/Button/index.js
+++ quiz-app/src/Components/Button/index.js
@@ -1,5 +1,28 @@
+const bStyles = {
+  padding: "10px 30px",
+  backgroundColor: "white",
+  color: "#6271a1",
+  borderRadius: "50px",
+  border: "none",
+  fontSize: "20px",
+  marginTop: "20px",
+};
+
 function Button({ value, dispatch, correctAnswer }) {
-	return <button onClick={() => dispatch({ type: value, correctAnswer: correctAnswer, givenAnswer: value})}>{value}</button>;
+  return (
+    <button
+      style={bStyles}
+      onClick={() =>
+        dispatch({
+          type: value,
+          correctAnswer: correctAnswer,
+          givenAnswer: value,
+        })
+      }
+    >
+      {value}
+    </button>
+  );
 }
 
 export default Button;
