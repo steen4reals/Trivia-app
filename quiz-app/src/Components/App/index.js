68e165e3cf071446b76f4b435d288e45c70c56d5
Liam Morley <liammorley123@live.co.uk>
2021-02-19 11:45:47 +0000

    pass onclick as props maybe - set up reducer

--- quiz-app/src/Components/App/index.js
+++ quiz-app/src/Components/App/index.js
@@ -1,22 +1,30 @@
 import "./App.css";
 import Screen from "../Screen/index";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import Button from "../Button";
 import useFetch from "../../hooks/useFetch";
 
 function App() {
-  //intial states
-  // const [index, setIndex] = useState(0);
-  //get 10 Qs on mount
+  function reducer(state, action) {
+    switch (action.type) {
+      case "True":
+        return index + 1;
+      case "False":
+        return index - 1;
+      default:
+        return state;
+    }
+  }
 
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [data, getNextData] = useFetch();
   return (
     <div className="App">
       <h1>Trials of Mt. Olympus</h1>
       <Screen question={data.question} />
       <div className="button-container">
-        <Button text= "True"/>
-        <Button text ="False"/>
+        <Button text="True" onClick={() => dispatch((type: "True"))} />
+        <Button text="False" onClick={() => dispatch((type: "False"))} />
       </div>
     </div>
   );
